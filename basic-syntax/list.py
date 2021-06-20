e530a338255474dde5e85b079e390138377f883f
hathatinggroot <hathatinggroot@gmail.com>
2021-06-20 15:56:58 +0900

    Initial commit

---
+++ basic-syntax/list.py
@@ -0,0 +1,14 @@
+# 1. list 병합
+mergedList = [1,2,3] + [4,5]
+print(mergedList)
+
+# 2. list comprehension
+nums = [i for i in range(10)]
+print(nums)
+
+odd_nums = [i for i in range(10) if i%2 == 1]
+print(odd_nums)
+
+table = [[0]*3 for _ in range(3)]
+print(table)
+
